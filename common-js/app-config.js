c054d008fc8ea7712798dec1b954cdffeaab6070
Sergey Matiyasevich <foxrandom.com@gmail.com>
2015-06-22 14:16:37 +0300

    navigation & color change

--- common-js/app-config.js
+++ common-js/app-config.js
@@ -2,8 +2,36 @@ var appConfig = {
 
     sections: [
         {
-
+            path: 'about',
+            menuItem: 'ABOUT',
+            pageTitle: 'About',
+            icon: 'mdi-candycane'
+        },
+        {
+            path: 'projects',
+            menuItem: 'PROJECTS',
+            pageTitle: 'Projects',
+            icon: 'mdi-briefcase'
+        },
+        {
+            path: 'experiments',
+            menuItem: 'EXPERIMENTS',
+            pageTitle: 'Experiments',
+            icon: 'mdi-beaker'
+        },
+        {
+            path: 'blog',
+            menuItem: 'BLOG',
+            pageTitle: 'Blog',
+            icon: 'mdi-tumblr'
+        },
+        {
+            path: 'feed',
+            menuItem: 'FEED',
+            pageTitle: 'Feed',
+            icon: 'mdi-television-guide'
         }
+
     ],
 
     core: ''
